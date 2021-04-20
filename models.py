dd7b9cea09be6d905eee387f92b238b720336279
IFFranciscoME <if.francisco.me@gmail.com>
2021-04-20 13:06:33 -0500

    Initial foundation for project structure

---
+++ models.py
@@ -0,0 +1,17 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- Project: LucidNet                                                                                   -- #
+# -- Description: A Lightweight Framework for Transparent and Interpretable FeedForward Neural Net       -- #
+# -- models.py: python script with Machine Learning Models                                               -- #
+# -- Author: IFFranciscoME - if.francisco.me@gmail.com                                                   -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- Repository: https://github.com/IFFranciscoME/LucidNet                                               -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
+# ------------------------------------------------------------------- FEEDFORWARD MULTILAYER PERECEPTRON -- #
+# ------------------------------------------------------------------------------------------------------ -- #
+
+# ---------------------------------------------------------------------------------- LOGISTIC REGRESSION -- #
+# ------------------------------------------------------------------------------------------------------ -- #
---
+++ visualizations.py
@@ -0,0 +1,11 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- Project: LucidNet                                                                                   -- #
+# -- Description: A Lightweight Framework for Transparent and Interpretable FeedForward Neural Net       -- #
+# -- visualizations.py: python script with visualization functions                                       -- #
+# -- Author: IFFranciscoME - if.francisco.me@gmail.com                                                   -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- Repository: https://github.com/IFFranciscoME/LucidNet                                               -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
