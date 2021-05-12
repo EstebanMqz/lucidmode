7d97bf2bedbef50b2237e2512c2d486ffa88452a
IFFranciscoME <if.francisco.me@gmail.com>
2021-05-11 23:37:26 -0500

    progress on publishing package

---
+++ lucidmode/__init__.py
@@ -0,0 +1,8 @@
+"""Transparent and Interpretable Machine Learning Models in Python, with a keras inspired API
+
+``lucidmode`` is a set of interpretable Machine Learning Models.
+
+"""
+__version__ = 'v0.4-beta1.0'
+
+__all__ = ['functions', 'models', 'propagate', 'regularization']
