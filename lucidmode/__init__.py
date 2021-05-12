f4f53fc1e8cdffee8ad14342082790294243153b
IFFranciscoME <if.francisco.me@gmail.com>
2021-05-12 00:52:22 -0500

    version

--- lucidmode/__init__.py
+++ lucidmode/__init__.py
@@ -3,6 +3,6 @@
 ``lucidmode`` is a set of interpretable Machine Learning Models.
 
 """
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 
 __all__ = ['functions', 'models', 'propagate', 'regularization']
