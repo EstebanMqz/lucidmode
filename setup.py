f772bfb6ca092e205fe2eff600149ffbe4ea0fdf
IFFranciscoME <if.francisco.me@gmail.com>
2021-05-12 22:54:00 -0500

    typo in setup.py

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(where='lu', exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=find_packages(where='lucidmode', exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
 
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
